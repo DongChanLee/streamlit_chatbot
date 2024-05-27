2db4120d8f0c4ceb9701f33994360450fd03c817
10150682 <dongchan.lee@kt.com>
2024-05-27 10:27:27 +0900

    first commit

---
+++ Home.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="DC AI STUDIO",
+    page_icon="🤖",
+)
+
+
+# st.subheader("DC Genrative AI STUDIO")
+
+st.markdown(
+    """
+# 당신을 위한 LLM 어플리케이션
+            
+#### DocumentGPT: 전문성 있는 정보, 내부 정보를 기반으로 답변하는 app
+#### QuizGPT: 질문에 대한 정확한 내용으로 답변하는 app
+            
+사용 가능한 어플리케이션 목록:
+            
+- [x] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
